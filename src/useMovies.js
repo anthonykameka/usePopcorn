0d681e54bf248b61c092353a125c4c0de8de76d3
“Anthony <antkameka@gmail.com>
2023-06-29 08:35:40 -0400

    update to URL http --> https

--- src/useMovies.js
+++ src/useMovies.js
@@ -16,7 +16,7 @@ export const useMovies = (query) => {
         setIsLoading(true);
         setError(null);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
           { signal: controller.signal }
         );
 
